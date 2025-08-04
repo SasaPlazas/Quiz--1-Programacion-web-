62257b786a2087c817c0b6db6ff2cf31a3848604
Sara Plazas Ramirez <1110364438@u.icesi.edu.co>
2025-08-04 17:45:55 -0500

    first

---
+++ src/index.js
@@ -0,0 +1,20 @@
+
+
+let word = ["gato", "perro", "juanito", "macarena"];
+// return words = Math.floor(Math.random() * 4);
+
+console.log(word);
+
+function shadow(word) {
+    word.split().find().join()
+}
+
+// const shadowLetter=  
+
+
+
+const inicial = document.getElementById('form'); 
+
+
+
+
