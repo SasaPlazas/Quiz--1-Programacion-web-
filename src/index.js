8c02d4bd2a109e8b90b5fecf0978230ec66fd48c
Sara Plazas Ramirez <1110364438@u.icesi.edu.co>
2025-08-04 18:25:08 -0500

    seconddd

--- src/index.js
+++ src/index.js
@@ -1,19 +1,32 @@
 
 
 let word = ["gato", "perro", "juanito", "macarena"];
-// return words = Math.floor(Math.random() * 4);
-
+const selectedword = Math.floor(Math.random() * 4);
 console.log(word);
 
-function shadow(word) {
-    word.split().find().join()
-}
+const nrearray = word[selectedword].split('')
+shadowLetter = nrearray.splice(2, 1);
+console.log(nrearray);
 
-// const shadowLetter=  
 
+const theWords = nrearray.document.getElementById('letter');
+
+theWords.innerHTML =`
+        <h1>${this.nrearray}</h1>
+`
 
 
 const inicial = document.getElementById('form'); 
+const result = inicial; 
+
+
+inicial.addEventListener("submit", () => {
+if (word === nrearray) {
+    alert('correct')
+} else {
+    alert ('incorrect')
+}
+})
 
 
 
